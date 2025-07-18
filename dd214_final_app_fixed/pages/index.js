2d5de64da4798c585294a377ff4beee71edc3862
terrylong46 <terrylong46@yahoo.com>
2025-07-18 18:18:57 -0500

    Add files via upload

---
+++ dd214_final_app_fixed/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Welcome to DD214 Update</h1>
+      <p>This is your homepage. Use the navigation bar to sign up or log in.</p>
+    </div>
+  );
+}
